3f989f5d2eddbe93a03764b24d695a9e63352544
saimus <saimus.dev@gmail.com>
2019-11-10 20:13:16 +0100

    Fixed directory traversing during script

--- third-party/prepare_dependencies.sh
+++ third-party/prepare_dependencies.sh
@@ -5,9 +5,11 @@ build_subfolder=tensorflow/tensorflow/lite/tools/make
 if [ ! -d tensorflow ]; then
     git clone "https://github.com/tensorflow/tensorflow.git"
 fi
+pushd $PWD >/dev/null
 cd tensorflow && git checkout $revision && cd ..
-#source "${build_subfolder}/download_dependencies.sh"
+source "${build_subfolder}/download_dependencies.sh"
+popd >/dev/null
 for dependency in $(ls -1 "${build_subfolder}/downloads"); do
-    mkdir -p $dependency
-    cp -rv "${build_subfolder}/downloads/${dependency}"/* $dependency
+    mkdir -p $dependency/source
+    cp -rv "${build_subfolder}/downloads/${dependency}"/* $dependency/source
 done
